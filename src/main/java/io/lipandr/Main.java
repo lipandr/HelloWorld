047f9fb4039e92dd8afcda863b76b755f936b663
lipandr <commit@lipandr.io>
2020-12-07 19:32:53 +0300

    first commit

--- src/main/java/io/lipandr/Main.java
+++ src/main/java/io/lipandr/Main.java
@@ -2,6 +2,8 @@ package io.lipandr;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        System.out.println("First commit");
     }
 }
